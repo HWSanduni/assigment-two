64e105c313411d57af2a59049f138446ba016333
HWSanduni <sandushashikala96@gmail.com>
2020-07-30 23:39:51 +0530

    Create QureyDao And CrudDao

--- src/dao/SuperDAO.java
+++ src/dao/SuperDAO.java
@@ -5,12 +5,6 @@ import entity.SuperEntity;
 import java.io.Serializable;
 import java.util.List;
 
-public interface SuperDAO <T extends SuperEntity,ID extends Serializable> {
-
-    public List<T> findAll();
-    public  T find(ID pk);
-    public  boolean save (T obj);
-    public  boolean update (T obj);
-    public  boolean delete (ID pk);
+public interface SuperDAO {
 
 }
--- src/dao/custom/OrderDetailDAO.java
+++ src/dao/custom/OrderDetailDAO.java
@@ -1,11 +1,12 @@
 package dao.custom;
 
+import dao.CrudDAO;
 import dao.SuperDAO;
 import entity.OrderDetail;
 import entity.OrderDetailPK;
 
 import java.util.List;
 
-public interface OrderDetailDAO extends SuperDAO<OrderDetail,OrderDetailPK> {
+public interface OrderDetailDAO extends CrudDAO<OrderDetail,OrderDetailPK> {
 
 }
